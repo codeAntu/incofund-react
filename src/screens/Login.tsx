ab16e60e84c2bff4241d72b0b4c260edacac231c
Ananta Karmakar <codeantu@gmail.com>
2024-03-17 16:33:59 +0530

    font and 3 icon added , login ui improved

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -2,52 +2,54 @@ import { Phone } from 'lucide-react'
 
 export default function Login() {
   return (
-    <div className='flex min-h-[100dvh] flex-col justify-center gap-8 p-5'>
-      <div className='flex flex-col gap-10'>
-        <div className='bg-accent p-8 text-3xl'>Helo</div>
-        <div className='flex flex-col items-center justify-center gap-2 border'>
-          <div className='text-3xl font-medium text-accent'>Welcome</div>
-          <div className='text-sm font-medium opacity-55'>Welcome to Incogroup investing app</div>
-        </div>
-      </div>
-      <div className='flex flex-col gap-3'>
-        <div className='flex flex-col gap-3'>
-          <div className='flex flex-col gap-0.5'>
-            <div className='px-1 text-sm font-medium opacity-50'>Phone Number</div>
-            <div className='flex w-full items-center rounded-lg border border-accent bg-accent/10 px-2'>
-              <Phone className='h-7 w-7 rounded-full bg-accent p-1.5 text-white ' />
-              <input
-                type='text'
-                className='w-full rounded-lg bg-transparent p-4 text-sm outline-none'
-                placeholder='Enter your phone number'
-              />
-            </div>
-          </div>
-          <div className='flex flex-col gap-0.5'>
-            <div className='px-1 text-sm font-medium opacity-50'>Phone Number</div>
-            <div className='flex w-full items-center rounded-lg border border-accent bg-accent/10 px-2'>
-              <Phone className='h-7 w-7 rounded-full bg-accent p-1.5 text-white ' />
-              <input
-                type='text'
-                className='w-full rounded-lg bg-transparent p-3.5 text-sm outline-none'
-                placeholder='Enter your phone number'
-              />
-            </div>
-          </div>
-          <div className='px-2 pb-1 text-right text-sm text-accent opacity-90'>
-            <a href=''>forget password?</a>
+    <div className='font-Montserrat flex min-h-[100dvh] w-full items-center justify-center'>
+      <div className='flex w-full max-w-[400px] flex-col justify-center gap-16 p-5 '>
+        <div className='flex flex-col items-center gap-24'>
+          <img src='./icons/appIcon.svg' alt='' className='w-[60%] pt-6' />
+          <div className='flex flex-col items-center justify-center gap-2'>
+            <div className='text-3xl font-semibold text-accent'>Welcome</div>
+            <div className='text-sm font-semibold opacity-50'>Welcome to Incogroup investing app</div>
           </div>
         </div>
-        <div className='mb-10 flex flex-col gap-2'>
-          <button className='w-full rounded-xl bg-accent p-3.5 text-sm font-medium text-white'> Sign In</button>
-          <div className='flex w-full items-center gap-1 px-2 text-black opacity-30'>
-            <hr className='w-full border-t-2 border-black' />
-            <p className='text-xs '>Or</p>
-            <hr className='w-full border-t-2 border-black' />
+        <div className='flex flex-col gap-10'>
+          <div className='flex flex-col gap-3'>
+            <div className='flex flex-col gap-1 '>
+              <div className='text-sm font-semibold opacity-45'>Phone Number</div>
+              <div className='flex w-full items-center rounded-[14px] border-[1.5px] border-accent bg-accent/5 px-3.5'>
+                <img src='./icons/phone.svg' alt='' className='w-[22px]' />
+                <input
+                  type='text'
+                  className='w-full bg-transparent px-3 py-4 text-sm font-semibold text-black/80 outline-none '
+                  placeholder='Enter your phone number'
+                />
+              </div>
+            </div>
+            <div className='flex flex-col gap-1 '>
+              <div className='text-sm font-semibold opacity-45'>Password</div>
+              <div className='flex w-full items-center rounded-[14px] border-[1.5px] border-accent bg-accent/5 px-3.5'>
+                <img src='./icons/key.svg' alt='' className='w-[22px]' />
+                <input
+                  type='password'
+                  className='w-full bg-transparent px-3 py-4 text-sm font-semibold text-black/80 outline-none '
+                  placeholder='Enter your phone number'
+                />
+              </div>
+            </div>
+            <div className='px-2 pb-1 text-right text-sm text-accent opacity-90'>
+              <a href=''>Forget password?</a>
+            </div>
           </div>
-          <button className='w-full rounded-xl border-2 border-accent p-3.5 text-sm font-medium text-accent '>
-            Register
-          </button>
+          <div className='flex flex-col gap-3'>
+            <button className='w-full rounded-[14px] bg-accent p-3.5 text-sm font-medium text-white'> Log In</button>
+            <div className='flex w-full items-center gap-1 px-2 '>
+              <hr className='w-full border-[0.5px] border-black/20' />
+              <p className='text-xs text-black/60'>Or</p>
+              <hr className='w-full border-[0.5px] border-black/20' />
+            </div>
+            <button className='w-full rounded-[14px] border-[1.5px] border-accent bg-accent/5 p-3.5 text-sm font-medium text-accent'>
+              Register
+            </button>
+          </div>
         </div>
       </div>
     </div>
