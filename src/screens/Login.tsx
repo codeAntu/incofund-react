36616d7afaab435d2532ad131369dbef81e767b4
Ananta Karmakar <codeantu@gmail.com>
2024-03-17 12:08:20 +0530

    tailwind problem fixed

---
+++ src/screens/Login.tsx
@@ -0,0 +1,10 @@
+export default function Login() {
+  return (
+    <div className="bg-black ">
+      <div>
+        <div className="text-white text-3xl">Helo</div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
