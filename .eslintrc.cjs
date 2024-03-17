d0ce79c39939ff476fd18c2118c66779fdf4569b
Abinash Karmakar <codeAbinash@gmail.com>
2024-03-17 13:23:13 +0530

    Add prettier and eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,18 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react-native/no-inline-styles': 'off',
+    'prettier/prettier': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'jsx-quotes': ['warn', 'prefer-single'],
+    semi: ['warn', 'never'],
+    curly: ['off', 'never'],
   },
 }
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,60 +1,55 @@
-import { Key, Phone } from "lucide-react";
+import { Phone } from 'lucide-react'
 
 export default function Login() {
   return (
-    <div className="min-h-[100dvh] flex flex-col p-5 justify-center gap-8">
-      <div className="flex flex-col gap-10">
-        <div className="text-3xl p-8 bg-accent">Helo</div>
-        <div className="flex flex-col justify-center border items-center gap-2">
-          <div className="text-3xl text-accent font-medium">Welcome</div>
-          <div className="opacity-55 font-medium text-sm">
-            Welcome to Incogroup investing app
-          </div>
+    <div className='flex min-h-[100dvh] flex-col justify-center gap-8 p-5'>
+      <div className='flex flex-col gap-10'>
+        <div className='bg-accent p-8 text-3xl'>Helo</div>
+        <div className='flex flex-col items-center justify-center gap-2 border'>
+          <div className='text-3xl font-medium text-accent'>Welcome</div>
+          <div className='text-sm font-medium opacity-55'>Welcome to Incogroup investing app</div>
         </div>
       </div>
-      <div className="flex flex-col gap-3">
-        <div className="flex flex-col gap-3">
-          <div className="flex flex-col gap-0.5">
-            <div className="text-sm opacity-50 font-medium px-1">Phone Number</div>
-            <div className="bg-accent/10 border border-accent rounded-lg w-full flex items-center px-2">
-              <Phone className="w-7 h-7 text-white bg-accent rounded-full p-1.5 " />
+      <div className='flex flex-col gap-3'>
+        <div className='flex flex-col gap-3'>
+          <div className='flex flex-col gap-0.5'>
+            <div className='px-1 text-sm font-medium opacity-50'>Phone Number</div>
+            <div className='flex w-full items-center rounded-lg border border-accent bg-accent/10 px-2'>
+              <Phone className='h-7 w-7 rounded-full bg-accent p-1.5 text-white ' />
               <input
-                type="text"
-                className="bg-transparent w-full p-4 outline-none rounded-lg text-sm"
-                placeholder="Enter your phone number"
+                type='text'
+                className='w-full rounded-lg bg-transparent p-4 text-sm outline-none'
+                placeholder='Enter your phone number'
               />
             </div>
           </div>
-          <div className="flex flex-col gap-0.5">
-            <div className="text-sm opacity-50 font-medium px-1">Phone Number</div>
-            <div className="bg-accent/10 border border-accent rounded-lg w-full flex items-center px-2">
-              <Phone className="w-7 h-7 text-white bg-accent rounded-full p-1.5 " />
+          <div className='flex flex-col gap-0.5'>
+            <div className='px-1 text-sm font-medium opacity-50'>Phone Number</div>
+            <div className='flex w-full items-center rounded-lg border border-accent bg-accent/10 px-2'>
+              <Phone className='h-7 w-7 rounded-full bg-accent p-1.5 text-white ' />
               <input
-                type="text"
-                className="bg-transparent w-full p-3.5 outline-none rounded-lg text-sm"
-                placeholder="Enter your phone number"
+                type='text'
+                className='w-full rounded-lg bg-transparent p-3.5 text-sm outline-none'
+                placeholder='Enter your phone number'
               />
             </div>
           </div>
-          <div className="text-right text-sm text-accent px-2 pb-1 opacity-90">
-            <a href="">forget password?</a>
+          <div className='px-2 pb-1 text-right text-sm text-accent opacity-90'>
+            <a href=''>forget password?</a>
           </div>
         </div>
-        <div className="flex flex-col gap-2 mb-10">
-          <button className="bg-accent p-3.5 text-white w-full rounded-xl text-sm font-medium">
-            {" "}
-            Sign In
-          </button>
-          <div className="flex w-full text-black items-center opacity-30 px-2 gap-1">
-            <hr className="border-t-2 border-black w-full" />
-            <p className="text-xs ">Or</p>
-            <hr className="border-t-2 border-black w-full" />
+        <div className='mb-10 flex flex-col gap-2'>
+          <button className='w-full rounded-xl bg-accent p-3.5 text-sm font-medium text-white'> Sign In</button>
+          <div className='flex w-full items-center gap-1 px-2 text-black opacity-30'>
+            <hr className='w-full border-t-2 border-black' />
+            <p className='text-xs '>Or</p>
+            <hr className='w-full border-t-2 border-black' />
           </div>
-          <button className="p-3.5 w-full rounded-xl text-sm font-medium border-2 border-accent text-accent ">
+          <button className='w-full rounded-xl border-2 border-accent p-3.5 text-sm font-medium text-accent '>
             Register
           </button>
         </div>
       </div>
     </div>
-  );
+  )
 }
