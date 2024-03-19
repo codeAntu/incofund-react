0c88bceffc04ef3966f23fec780328ab9781b02d
Ananta Karmakar <codeantu@gmail.com>
2024-03-20 00:51:02 +0530

    all pages complete

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,11 @@ import MyRecords from './screens/MyRecords.tsx'
 import MyCard from './screens/MyCard.tsx'
 import SplashScreen from './screens/SplashScreen.tsx'
 import Product from './screens/Product.tsx'
+import Home from './screens/Home.tsx'
+import Refer from './screens/Refer.tsx'
+import PopOver from './screens/PopOver.tsx'
+import Nav from './screens/Nav.tsx'
+import Menu from './screens/Menu.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -29,6 +34,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     {/* <MyRecords /> */}
     {/* <MyCard /> */}
     {/* <SplashScreen /> */}
-    <Product />
+    {/* <Product /> */}
+    {/* <Home /> */}
+    {/* <Refer /> */}
+    {/* <PopOver /> */}
+    {/* <Nav /> */}
+    <Menu />
   </React.StrictMode>,
 )
